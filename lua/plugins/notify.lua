24e68ed3232a6e33ce84e3c9341cec55db518621
DACHXY <Danny10132024@gmail.com>
2024-04-09 14:37:03 +0800

    add nvim-notify and venv-selector config

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      stages = "slide",
+      render = "wrapped-compact",
+      timeout = 2000,
+      top_down = false,
+      fps = 40,
+    },
+  },
+}
